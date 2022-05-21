2f403560aba78059d5b4595522e4c39a10505e48
shadowdotpy <derjohannes.bock@gmail.com>
2022-05-22 01:57:54 +0200

    Change 001

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function loginSubmit() {
-    console.log("Logged in")
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    if (username == 'admin' && password == 'krassespasswort69') {
+        alert('Das Passwort ist richtig! Hurra!')
+    } else {
+        alert('Passwort falsch!');
+    }
 }
